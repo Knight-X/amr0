b33fcb7fa246195aed0881068747873d4b9d8c0f
kazami <kazamizero@gamil.com>
2016-03-13 00:13:22 +0800

    title: put the database which can store data

--- data.py
+++ data.py
@@ -1,5 +1,12 @@
 import psycopg2
 
-conn = psycopg2.connect(database="testdb", user="kazami", password="dj4xji4", host="127.0.0.1", port="5432")
+conn = psycopg2.connect(database="kazami", user="kazami", password="dj4xji4", host="127.0.0.1", port="5432")
 
+cur = conn.cursor()
+cur.execute("SELECT * from users")
+
+rows = cur.fetchall()
+print("\nShow me the databases:\n")
+for row in rows:
+	print(" ", row[0], " ", row[1])
 print("opened")
