d3934bddabc9f4cf78371d000ac3eaf814b21c20
kazami <kazamizero@gamil.com>
2016-03-19 11:46:14 +0800

    title: data edited for storing query in postgresql

--- data.py
+++ data.py
@@ -1,8 +1,11 @@
 import psycopg2
+import json
 
 conn = psycopg2.connect(database="kazami", user="kazami", password="dj4xji4", host="127.0.0.1", port="5432")
 
 cur = conn.cursor()
+cur.execute("INSERT INTO users(name, love) VALUES (%s, %s)", ("kazami", json.JSONEncoder().encode({"FOOD":{"LUNCH":["X", "Y"], "C":"Y"}})))
+
 cur.execute("SELECT * from users")
 
 rows = cur.fetchall()
@@ -10,3 +13,5 @@ print("\nShow me the databases:\n")
 for row in rows:
 	print(" ", row[0], " ", row[1])
 print("opened")
+conn.commit()
+cur.close()
